ee6982184f743c753d6b379b58a16bc47768c431
LilaFridley <LilaFridley@dhcp-18-111-58-203.dyn.mit.edu>
2017-05-01 18:07:42 -0400

    LilaFrontEnd

---
+++ LilaFrontEnd/datatable.js
@@ -0,0 +1,56 @@
+
+
+var datatable = [
+    {
+        'ItemID': 0,
+        'ItemName': 'drink0',
+        'ItemCost': '0', // will need to convert number to dollar value in display
+        'ItemDescription': 'crisp beer'
+        
+    },
+    {
+        'ItemID': 1,
+        'ItemName': 'drink1',
+        'ItemCost': '1', 
+        'ItemDescription': 'crisp beer'
+    },
+    {
+        'ItemID': 2,
+        'ItemName': 'drink2',
+        'ItemCost': '2', 
+        'ItemDescription': 'crisp beer'
+    },
+    {
+        'ItemID': 3,
+        'ItemName': 'drink3',
+        'ItemCost': '3', 
+        'ItemDescription': 'crisp beer'
+    },
+    {
+        'ItemID': 4,
+        'ItemName': 'drink4',
+        'ItemCost': '4', 
+        'ItemDescription': 'crisp beer'
+    },
+    {
+        'ItemID': 5,
+        'ItemName': 'drink5',
+        'ItemCost': '5', 
+        'ItemDescription': 'crisp beer'
+    },
+    {
+        'ItemID': 6,
+        'ItemName': 'drink6',
+        'ItemCost': '6', 
+        'ItemDescription': 'crisp beer'
+    }
+
+];
+
+// $('#table').bootstrapTable(data: 'mydata');
+
+// $(function () {
+//     $('#table').bootstrapTable({
+//         data: mydata
+//     });
+// });
